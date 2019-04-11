9bbf699ef0e848522d6e17267423a6cf82cc2276
Cory Gargas <cbgargas@uark.edu>
2019-04-11 13:49:27 -0500

    fixing everything but stupid name game and need to fix the writing of original line

--- fasta-fixer.py
+++ fasta-fixer.py
@@ -4,44 +4,40 @@ import argparse
 from Bio import SeqIO
 
 #create and argumentparser object('parser') that will hold all info to parse the cmd line
-parser = argparse.ArgumentParser(description = 'imports and corrects shitty fasta files')
+parser = argparse.ArgumentParser(description = 'Imports and corrects shitty fasta files (e.g., Silva LTP fasta files).')
 
 #adding positional arguments to parser
 parser.add_argument('infile', help='.fasta file you wish to parse by .gff features', type=str)
+
+#adding optional arguments to parser
+parser.add_argument('-f1','--format1', help='format of input file', type=str, default='fasta')
+parser.add_argument('-f2','--format2', help='desired output format for output file', type=str, default='fasta-2line')
 parser.add_argument('-o','--outfile', help='name for your output file', type=str, default='output.fa')
 
 #parse the cmd line arguments
 args = parser.parse_args()
 
 #open your output file for writing
-with open(args.outfile, 'w') as output:
+with open('output1.fa', 'w') as output1:
     #open your input fasta file
-    with open(args.infile) as fasta:
+    with open(args.infile) as fasta: 
         #read in fasta file with SeqIO
-        fsa = SeqIO.parse(fasta, 'fasta')
-
+        fsa = SeqIO.convert(fasta, args.format1, output1, args.format2)
         #gives available options for an object
-        print(dir(fsa))
-
-
-        #add an if/then statement for sequence headers
-#        for line in fsa:
-#            if line.find('>'):
-#                line.replace(' ', '_')
-#                line.replace('\t', '_')
-#                outfile.write(line)
-#            else:
-#                line.replace(' ', '')
-#                output.write(line)
-
-#        out = SeqIO.write(fsa, output, 'fasta-2line')
+        #print(dir(fsa))
+        with open(args.outfile, 'w') as output2:
+            #open your output1 file for writing
+            #with open('output1.fa', 'r') as fasta2:
+            #add an if/then statement for sequence headers
+            for line in output1:
+                output2.write(line.replace('.', '-'))
+                output2.write(line.replace('\t'or' ', '_'))
 
 
 
-
-
-
-#close the files 		
-output.close()
+#close the files 	
+output1.close()
 fasta.close()
+output2.close()
+fasta2.close()
 
